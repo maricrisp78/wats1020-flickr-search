c75bebe781cfc9ed01b7aee7aa8dfc4c16cc43aa
marieashley_78 <marieashley_78@hotmail.com>
2015-08-04 01:12:53 -0400

    commit flickr search

--- js/main.js
+++ js/main.js
@@ -5,8 +5,77 @@
 // of tags and view the images that are found.
 //
 // Allow users to click the images to see a larger version with more information.
-$(document).on('ready', function(){
+	
+	
+	$(document).on('ready', function(){
+  var searchImages = function(tags) {
+    var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    console.log(tags);
+    $('#images').innerHTML = '<li class="search-throbber">Searching...</li>';
+    $.getJSON( flickrAPI, {
+      tags: tags,
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+      $('#images').empty();
+      $('h1.search-title').first()[0].innerHTML = "Search for: " + tags;
+      $.each( data.items, function( i, item ) {
+        var newListItem = $("<li>");
+        // If you're not doing the modal, then show info about the image.
+        var newTitle = $('<p class="image-title">').html(item.title).appendTo(newListItem);
+        var newDate = $('<p class="image-date">').text(item.date_taken).appendTo(newListItem);
+        var newDescription = $('<p class="image-description">').html(item.description).appendTo(newListItem);
+        var newLink = $('<a>').attr('href', item.link).text('View on Flickr.').appendTo(newListItem);
+
+        // Button only needed if you're doing the modal
+        var newButton = $("<button class='btn btn-sm btn-primary'>enlarge</button>").attr({
+          'data-title': item.title,
+          'data-toggle': "modal",
+          'data-target': "#infoModal",
+          'data-imgsrc': item.media.m,
+          'data-description': item.description,
+          'type': "button"
+        }).appendTo(newListItem);
+        newListItem.appendTo( "#images" );
+        if ( i === 100 ) {
+          return false;
+        }
+      });
+    });
+  };
+
+  $('button.search').on('click', function(event){
+    event.preventDefault();
+    var searchTextInput = $(event.target.parentElement).find('input[name="searchText"]')[0];
+    console.log(searchTextInput);
+    searchImages(searchTextInput.value);
+  });
+
+  $('#infoModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var title = button.data('title'); // Extract info from data-* attributes
+    var imgSrc = button.data('imgsrc');
+    var imageDescription = button.data('description');
+
+    // Update the modal's content. We'll use jQuery here.
+    var modal = $(this);
+    modal.find('.modal-title').html(title);
+    var modalBody = modal.find('.modal-body');
+    modalBody.empty();
+    var modalDescription = $("<p class='image-description'>").html(imageDescription).appendTo(modalBody);
+  });
+
+});
     // Place your code here, inside the document ready handler.
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
     // Create a function called `searchImages()`. This function will handle the
     // process of taking a user's search terms and sending them to Flickr for a
@@ -45,5 +114,3 @@ $(document).on('ready', function(){
     // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
 
 
-
-});
